96b20109a81a88379630b582365e9773d9f87d8f
XUMENG <1143349706@qq.com>
2017-02-22 22:12:01 +0800

    Add files via upload

---
+++ BeautifyCamera-v1Tests/BeautifyCamera_v1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  BeautifyCamera_v1Tests.swift
+//  BeautifyCamera-v1Tests
+//
+//  Created by pro on 16/6/19.
+//  Copyright © 2016年 MengXu. All rights reserved.
+//
+
+import XCTest
+@testable import BeautifyCamera_v1
+
+class BeautifyCamera_v1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
